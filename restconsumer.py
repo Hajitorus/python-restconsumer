8f45cf62868d623f4d8fe1e15ff305a011b874a3
Damian Bickhoff <d.bickhoff@griffith.edu.au>
2016-09-08 10:01:18 +1000

    Fix formatting.

--- restconsumer.py
+++ restconsumer.py
@@ -1,23 +1,23 @@
 import requests, json
 
-def append_to_url(base_url,param):
-    return "%s%s/" % (base_url,param)
+def append_to_url(base_url, param):
+    return "%s%s/" % (base_url, param)
 
 
 class RestConsumer(object):
 
-    def __init__(self,base_url,append_json=False,append_slash=False):
+    def __init__(self, base_url, append_json=False, append_slash=False):
         self.base_url = base_url if base_url[-1] == '/' else "%s%s" % (base_url,"/")
         self.append_json = append_json
         self.append_slash = append_slash
 
-    def __getattr__(self,key):
-        new_base = append_to_url(self.base_url,key)
+    def __getattr__(self, key):
+        new_base = append_to_url(self.base_url, key)
         return self.__class__(base_url=new_base,
                               append_json=self.append_json,
                               append_slash=self.append_slash)
     
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return self.__getattr__(key)
 
     def __call__(self, **kwargs):
@@ -26,24 +26,24 @@ class RestConsumer(object):
         if self.append_json:
             self.base_url = "%s%s" % (self.base_url,'.json')
         print "Calling %s" % self.base_url
-        return self.get(self.base_url,**kwargs)
+        return self.get(self.base_url, **kwargs)
 
-    def get(self,url,**kwargs):
-        r = requests.get(url,**kwargs)
+    def get(self, url, **kwargs):
+        r = requests.get(url, **kwargs)
         return json.loads(r.content)
 
-    def post(self,**kwargs):
+    def post(self, **kwargs):
         r = requests.post(**kwargs)
         return json.loads(r.content)
 
 
 if __name__=='__main__':
-    Twitter = RestConsumer(base_url='https://api.twitter.com/1',append_json=True)
+    Twitter = RestConsumer(base_url='https://api.twitter.com/1', append_json=True)
     Github = RestConsumer(base_url='https://api.github.com')
     Stackoverflow = RestConsumer(base_url='http://api.stackoverflow.com/1.1')
 
     from pprint import pprint
-    t = RestConsumer(base_url='https://api.twitter.com/1',append_json=True)
+    t = RestConsumer(base_url='https://api.twitter.com/1', append_json=True)
     public_timeline = t.statuses.public_timeline()
     pprint(public_timeline)
 
